3126d96117de74db3b5a5a78c0b09f7537d30629
JiangBaiyu <877136144@qq.com>
2021-11-06 20:20:32 +0800

    test2

--- src/com/company/designModel/proxy/dynamicProxy/Client.java
+++ src/com/company/designModel/proxy/dynamicProxy/Client.java
@@ -13,8 +13,6 @@ public class Client {
         //创建目标对象（被代理对象）
         ITeacherDao target = new TeacherDao();
 
-
-
         //给目标对象创建代理对象，可以转成ITeacherDao
         ITeacherDao proxyInstance = (ITeacherDao) new ProxyFactory(target).getProxyInstance();
 

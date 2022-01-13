bb978b95b983fdacc9bfd14354b079470b94a26f
JiangBaiyu <877136144@qq.com>
2022-01-13 18:50:10 +0800

    伪日志生成

---
+++ src/com/company/se/GenerateLog.java
@@ -0,0 +1,26 @@
+package com.company.se;
+
+import java.util.Random;
+
+/**
+ * @Description 生成模拟日志文件
+ * @Author Jiang
+ * @Date 2021/12/30 13:38
+ * @Version 1.0.0
+ */
+
+public class GenerateLog {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        long taskId = 0L;
+        int exectime = 0;
+
+        for (int i = 0; i < 20; i++) {
+            taskId = random.nextInt(10000)+1640830570000L;
+            exectime = random.nextInt(10) + 5;
+            String log = "taskId=" + taskId + " shardingNum=0 WithdrawService::withdraw exectime=" + exectime + " desc=end";
+            System.out.println(log);
+        }
+    }
+}
